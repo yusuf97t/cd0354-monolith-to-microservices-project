a044eacd9a5160b13c3d5e7338b91b49a9b8d4f3
yusidris <idrisoluwatobi97@gmail.com>
2022-08-07 07:41:06 -0400

    new

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=oluwatobi
+export POSTGRES_PASSWORD=yusufs97
+export POSTGRES_HOST=idris.can5pc534uae.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=arn:aws:s3:::tobiyusufdev
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
---
+++ solution/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=oluwatobi
+export POSTGRES_PASSWORD=yusufs97
+export POSTGRES_HOST=idris.can5pc534uae.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::tobiyusufdev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
